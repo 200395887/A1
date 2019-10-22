7fc6c2131257bd3dd9a4d59efb7c2ad80c3174c1
Akshay Bansal <43522054+200395887@users.noreply.github.com>
2019-10-21 23:04:48 -0400

    update4

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('product', { title: 'Express' });
 });
 
 module.exports = router;
